98ce65f4cdd6136155b07e62f67d2f1e571a6d14
gabystudioweb <mgabrielalozanog@gmail.com>
2021-06-17 19:17:59 -0300

    entrega de 1er ejercicio de javascript

---
+++ clase 1 -javascript/claseUno.js
@@ -0,0 +1,9 @@
+// Calcular promedio de 3 valores ingresados
+
+
+const valorUno=parseFloat(prompt("Ingrese el primer valor"));
+const valorDos=parseFloat(prompt("Ingrese el segundo valor"));
+const valorTres=parseFloat(prompt("Ingrese el tercer valor"));
+
+const p=(valorUno+valorDos+valorTres)/3;
+alert("El promedio es "+p);
