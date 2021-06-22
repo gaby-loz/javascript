ee6be63e4238162b068dde603e722237bc15cb50
gabystudioweb <mgabrielalozanog@gmail.com>
2021-06-22 20:23:34 -0300

    ejercicios de la clase 2 javascript

---
+++ operadores.js
@@ -0,0 +1,9 @@
+const nombreDeUsuario=prompt("Ingrese su nombre por favor");
+const clavePersonal=prompt("Ingrese su contraseña");
+
+if ((nombreDeUsuario=="GABY")||(nombreDeUsuario=="gaby")&&(clavePersonal==="gaby123")){
+console.log("Bienvenida Gaby");
+
+}else {
+    console.log("Error:datos incorrectos");
+}
