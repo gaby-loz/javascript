0c876fecfd21d150e80a47eb4f41d608250c6d88
gabystudioweb <mgabrielalozanog@gmail.com>
2021-07-01 20:43:10 -0300

    correccion de la funcion

--- clase4-javascript/funtions.js
+++ clase4-javascript/funtions.js
@@ -1,10 +1,8 @@
-
-//Calcular Iva a través de una función
-
 function calcularIva(){
     let precio = parseFloat(prompt("Ingresar el precio"));
-    let calcularIva = precio * 0.21;
-    console.log ("El precio con Iva es " + calcularIva);
+    let iva = precio * 0.21;
+    console.log ("El precio con Iva es " + iva);
+    return iva;
 } 
 
 calcularIva();
