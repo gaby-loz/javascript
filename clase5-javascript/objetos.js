2428799786f4df23ee4c418d3c4921c4b85019a7
gabystudioweb <mgabrielalozanog@gmail.com>
2021-07-06 21:19:05 -0300

    clase 5 javascript - objetos

--- clase5-javascript/objetos.js
+++ clase5-javascript/objetos.js
@@ -1,38 +1,34 @@
-// SIMULADOR ALTA DE PERSONAS
-class Persona{
-    //METODO CONSTRUCTOR: CON EL CREAMOS EL OBJETO
-    constructor(nuevoNombre, nuevaEdad,nuevoDNI){
-        this.nombre = nuevoNombre;
-        this.edad   = parseInt(nuevaEdad);
-        this.dni    = parseInt(nuevoDNI);
+class Producto {
+    constructor(nombre, tipo, precio) {
+        this.nombre  = nombre;
+        this.tipo = tipo;
+        this.precio  = parseFloat(precio);
+        
     }
-    //FUNCION QUE IMPRIME LA INFORMACION DE UN OBJETO CON CIERTO FORMATO
-    informacionPersona(){
-        return "NOMBRE " + this.nombre + " - EDAD " + this.edad + " - D.N.I " + this.dni + "\n" ;
+
+    iva() {
+        this.precio = this.precio + (this.precio * 0.21);
+        return(this.precio);
     }
-}
-//FUNCION PARA CREAR UN OBJETO "PERSONA" CAPTURANDO ENTRADAS DE USUARIO
-function crearPersona(){
-    let nuevoNombre = prompt("INGRESAR NOMBRES");
-    let nuevaEdad   = prompt("INGRESAR EDAD");
-    let nuevoDNI    = prompt("INGRESAR DNI");
-    return new Persona(nuevoNombre,nuevaEdad,nuevoDNI);
-}
-//CAPTURAMOS UNA ENTRADA PARA DETERMINA SI CREAMOS O NO UN OBJETO
-let seleccionarOpcion = prompt("INGRESAR (1) PARA CARGAR UNA PERSONA \n PRESIONAR OTRA TECLA PARA SALIR");
-let listadoPersonas   = [];
-//UTILIZAMOS UN BUCLE PARA CREAR UN OBJETO LLAMANDO A LA FUNCION CADA VEZ QUE LA ENTRADA ES 1
-while (seleccionarOpcion === "1"){
-    listadoPersonas.push(crearPersona());
-    seleccionarOpcion = prompt("INGRESAR (1) PARA CARGAR UNA PERSONA \n PRESIONAR OTRA TECLA PARA SALIR");
-    console.log(mensajeFinal.toUpperCase());
-}
-//SI HAY UN ELEMENTO EN EL ARRAY MOSTRAMOS EL DETALLE DE CADA ELEMENTO EN UNA ALERTA
-if (listadoPersonas.length > 0){
-    let mensajeFinal = "PERSONAS CARGADAS \n";
-    //UTILIZAMOS UN FOR..OF PARA OBTENER LA INFORMACION DE TODOS LOS OBJETOS CARGADOS
-    for (const persona of listadoPersonas) {
-        mensajeFinal += persona.informacionPersona();
+
+    mostrarProducto() {
+        return "Nombre: " + this.nombre + "\nTipo de producto: " + this.tipo + "\nPrecio del producto: " + this.precio + "\n";
     }
-    console.log(mensajeFinal.toUpperCase());
+    
 }
+
+function crearProducto(){
+    let nombre = prompt("INGRESAR NOMBRE DE PRODUCTO");
+    let tipo   = prompt("INGRESAR TIPO DE PRODUCTO");
+    let precio    = prompt("INGRESAR PRECIO DEL PRODUCTO");
+    return new Producto(nombre, tipo, precio);
+}
+
+
+const producto1 = (crearProducto());
+const producto2 = (crearProducto());
+producto1.iva();
+producto2.iva();
+console.log(producto1.mostrarProducto() + producto2.mostrarProducto());
+
+
