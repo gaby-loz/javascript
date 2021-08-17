ab4a77649130d718d63fc5a62b027cd53012ce79
gabystudioweb <mgabrielalozanog@gmail.com>
2021-08-17 19:37:03 -0300

    correccion simulador

--- clase6-javacript/simulador.js
+++ clase6-javacript/simulador.js
@@ -12,7 +12,7 @@ const listaDeInscritos=[];
 class cursos{
     constructor(nombreCurso,precioCurso,categoriaCurso, duracionCurso){
         this.nombreCurso=nombreCurso,
-        this.precioCurso=number(precioCurso),
+        this.precioCurso=parseFloat(precioCurso),
         this.categoriaCurso=categoriaCurso,
         this.duracionCurso=duracionCurso
     }
@@ -32,40 +32,39 @@ cursosDisponibles.push(curso1, curso2)
 function inscripcionCurso(){
 
     let entrada = prompt("¿En qué curso te interesa inscribirte?: 1.- Diferencia sexual, 2.- Introducción a Zizek");
-
-while(entrada!=ESC){
-    let contador=0;
-    switch(entrada){
-        case "Diferencia sexual":
-            if(contador <=10){
-                let suma= contador + 1;
-                let nombre = prompt("Ingrese su nombre");
-                let apellido = prompt("Ingrese su apellido");
-                let email = prompt("Ingrese su email");
-                let nuevoInscrito1 = new nuevoInscrito (nombre, apellido, email);
-                listaDeInscritos.push(nuevoInscrito1);
-                console.log("Hola " + nombre + "Te inscribirte exitosamente en el curso: Diferencia Sexual")
-            }else{
-                alert("Curso lleno")
-            }
-        break;
-
-        case "Intrudcción a Zizek":
-            if (contador<= 10){
-                let suma= contador + 1;
-                let nombre = prompt("Ingrese su nombre");
-                let apellido = prompt("Ingrese su apellido");
-                let email = prompt("Ingrese su email");
-                let nuevoInscrito1 = new nuevoInscrito (nombre, apellido, email);
-                listaDeInscritos.push(nuevoInscrito1);
-                console.log("Hola " + nombre + "Te inscribirte exitosamente en el curso: Introducción a Zizek")
-            }else{
-                alert("Curso lleno")
-            }
-        break;
-    
+    while(entrada!="ESC"){
+        let contador=0;
+        switch(entrada){
+            case "Diferencia sexual":
+                if(contador <=10){
+                    let suma= contador + 1;
+                    let nombre = prompt("Ingrese su nombre");
+                    let apellido = prompt("Ingrese su apellido");
+                    let email = prompt("Ingrese su email");
+                    let nuevoInscrito1 = new nuevoInscrito (nombre, apellido, email);
+                    listaDeInscritos.push(nuevoInscrito1);
+                    console.log("Hola " + nombre + ", " + "Te inscribiste exitosamente en el curso: Diferencia Sexual")
+                }else{
+                    alert("Curso lleno")
+                }
+            break;
+            case "Intrudcción a Zizek":
+                if (contador<= 10){
+                   let suma= contador + 1;
+                   let nombre = prompt("Ingrese su nombre");
+                   let apellido = prompt("Ingrese su apellido");
+                   let email = prompt("Ingrese su email");
+                   let nuevoInscrito1 = new nuevoInscrito (nombre, apellido, email);
+                   listaDeInscritos.push(nuevoInscrito1);
+                   console.log("Hola " + nombre + ", " + "Te inscribiste exitosamente en el curso: Introducción a Zizek")
+                }else{
+                   alert("Curso lleno")
+                }
+            break;
+        }
+        entrada = prompt("¿En qué curso te interesa inscribirte?: 1.- Diferencia sexual, 2.- Introducción a Zizek");
     }
 
 }
 
-inscripcionCurso()
+inscripcionCurso();
